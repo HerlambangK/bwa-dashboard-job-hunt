a1f7d2d1b1726fa8b9d7cc067ae2af323270b3c1
herlambang k <herlambangk25@gmail.com>
2023-09-25 21:48:15 +0700

    feat(db):init prisma orm, db railways, and supabase

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -49,5 +49,11 @@ export const teamFormSchema = z.object({
 })
 
 export const signInFormSchema = z.object({
-    
+    email:z.string({required_error:'Email is required'}).email({message: 'Email is invalid'}),
+    password:z.string({required_error:'Password is required'})
+})
+export const signUpFormSchema = z.object({
+    name:z.string({required_error:'Name is required'}),
+    email:z.string({required_error:'Email is required'}).email({message: 'Email is invalid'}),
+    password:z.string({required_error:'Password is required'})
 })
